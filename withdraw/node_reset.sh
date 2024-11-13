dddb58b6887490b65e3c6661940c7351c4cfc92a
suhard <63784117+suhard@users.noreply.github.com>
2024-11-13 13:20:28 +0100

    Update node_reset.sh

--- withdraw/node_reset.sh
+++ withdraw/node_reset.sh
@@ -4,4 +4,5 @@ kubeadm reset --force
 test -f /etc/default/kubelet && rm /etc/default/kubelet
 iptables -F && iptables -t nat -F && iptables -t mangle -F && \
 iptables -X && iptables -t nat -X && iptables -t mangle -X
+rm -r /etc/cni/net.d/
 systemctl restart containerd
