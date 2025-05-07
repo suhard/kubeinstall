acb9d5d28fb8824f45241508630a5e947ce734ef
suhard <63784117+suhard@users.noreply.github.com>
2025-05-07 14:39:01 +0200

    RIP WeaveWorks, Long Live WeaveNet!

--- deploy/weavenet_setup.sh
+++ deploy/weavenet_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 ## Apply WeaveNet CNI plugin
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+## Historical version (RIP WeaveWorks)
+## https://thenewstack.io/end-of-an-era-weaveworks-closes-shop-amid-cloud-native-turbulence/
+## kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+KUBEVER=$(kubectl version | base64 | tr -d '\n')
+kubectl apply -f https://reweave.azurewebsites.net/k8s/net?k8s-version=$KUBEVER
